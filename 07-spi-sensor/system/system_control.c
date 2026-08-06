20e8ec204a6eb54b9b788885e8abcd7cb4dba716
haikevins <nguyengochai2004@gmail.com>
2026-08-06 14:10:18 +0700

    update: 07-spi-sensor example

--- 07-spi-sensor/system/system_control.c
+++ 07-spi-sensor/system/system_control.c
@@ -4,10 +4,6 @@
 
 void system_idle(void)
 {
-    /*
-     * Keep the core in Run mode so ST-Link probes without an NRST
-     * connection can attach reliably after the firmware starts.
-     */
     __NOP();
 }
 
